84651b9a67bb20ee5b855cf4f74548441fdd1d15
MahaSabry5 <maha08526@gmail.com>
2021-11-18 18:12:01 +0200

    create wholeData view

--- index.php
+++ index.php
@@ -63,14 +63,17 @@ require 'dbConnect.php';
             FROM employee,department
             WHERE department.mgrssn=employee.ssn";
         $conn->query($view2);
-
-        $sql="SELECT  work_hours.name, work_hours.ssn , e.bdate ,super.fname as supervisor, department_mangers.dname ,  department_mangers.fname as dep_manage , work_hours.pname,work_hours.hours
+        $view3 = "CREATE VIEW IF NOT EXISTS employees_Data
+            AS 
+            SELECT  work_hours.name, work_hours.ssn , e.bdate ,super.fname as supervisor, department_mangers.dname ,  department_mangers.fname as dep_manage , work_hours.pname,work_hours.hours
 FROM employee as e
 INNER JOIN work_hours
 ON work_hours.ssn=e.ssn
 LEFT JOIN department_mangers
 ON e.dno = department_mangers.dnumber
 LEFT JOIN employee as super ON e.superssn = super.ssn";
+$conn->query($view3);
+        $sql="SELECT * From employees_Data";
         $records = $conn->query($sql);
 
         while($row = $records->fetch_assoc()) {  ?>
