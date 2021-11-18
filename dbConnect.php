e3f29b4d69674ca1a2087ce295a6ef6f5b5c3901
MahaSabry5 <maha08526@gmail.com>
2021-11-18 13:34:32 +0200

    view employees

--- dbConnect.php
+++ dbConnect.php
@@ -2,7 +2,9 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "test";
+//$dbname = "test";
+$dbname = "company";
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
